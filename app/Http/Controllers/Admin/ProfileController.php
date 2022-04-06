88fd0ae54360d60669f6e9660e6245901ce53cb5
arisa <arisatsutsumi223@gmail.com>
2022-04-06 01:39:09 +0000

    php16課題

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -17,7 +17,6 @@ class ProfileController extends Controller
       $profile = new Profile;
       $form = $request->all();
       
-      
       // フォームから送信されてきた_tokenを削除する
       unset($form['_token']);
       // データベースに保存する
@@ -25,10 +24,15 @@ class ProfileController extends Controller
       $profile->save();
         return redirect('admin/profile/create');
     }
-    public function edit()
-    {
-        return view('admin.profile.edit');
-    }
+    public function edit(Request $request)
+  {
+      // News Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      if (empty($profile)) {
+        abort(404);    
+      }
+      return view('admin.profile.edit', ['profile_form' => $profile]);
+  }
     public function update()
     {
         return redirect('admin/profile/edit');
